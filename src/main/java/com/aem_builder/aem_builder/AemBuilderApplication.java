3025d66267e71672b384a4052a2dd91bc26368b6
Nirbhai <nirbhaip@digiquadsolutions.com>
2025-07-18 11:47:08 +0530

    initial commit

---
+++ src/main/java/com/aem_builder/aem_builder/AemBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.aem_builder.aem_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AemBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aem_builder/aem_builder/AemBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aem_builder.aem_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AemBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
